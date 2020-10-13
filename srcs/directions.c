33f78f4a5913ebe4a878a589ce41e25f22cb66ad
Freely Kathryne <fkathryn@si-j4.kzn.21-school.ru>
2020-10-13 21:30:22 +0300

    redir

--- srcs/directions.c
+++ srcs/directions.c
@@ -6,7 +6,7 @@
 /*   By: fkathryn <fkathryn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 14:31:56 by qtamaril          #+#    #+#             */
-/*   Updated: 2020/10/13 20:44:36 by fkathryn         ###   ########.fr       */
+/*   Updated: 2020/10/13 21:29:57 by fkathryn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,24 @@ void	get_pipe_fd(t_fd *fd_pipe)
 	fd_pipe->pipe_flag = 1;
 }
 
+void			back_redir(char **line, t_fd *fd_pipe, t_list *env);
+
+void			double_redir(char **line, t_fd *fd_pipe, t_list *env)
+{
+	char *file_name;
+	int fd;
+
+	(*line) += 2;
+	while (ft_isspace(**line))
+		(*line)++;
+	file_name = parse_argument(line, env);
+	fd = open(file_name, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	// printf("%s\n", file_name);
+	free(file_name);
+	if (fd_pipe->stdout_write >= 0) //хз
+		fd_pipe->stdout_write = fd;
+}
+
 void			forward_redir(char **line, t_fd *fd_pipe, t_list *env)
 {
 	char *file_name;
@@ -35,7 +53,7 @@ void			forward_redir(char **line, t_fd *fd_pipe, t_list *env)
 	fd = open(file_name, O_CREAT | O_TRUNC | O_WRONLY, 0644);
 //	printf("%s\n", file_name);
 	free(file_name);
-	if (fd_pipe->stdout_write >= 0)
+	if (fd_pipe->stdout_write >= 0) //хз
 		fd_pipe->stdout_write = fd;
 }
 
@@ -49,14 +67,15 @@ void			get_redir_fd(char **line, t_fd *fd_pipe, t_list *env)
 	temp_line = *(line);
 	while (*temp_line)
 	{
+		//подменить fd
 		while (ft_isspace(*temp_line))
 			temp_line++;
 		if (!(ft_strncmp(temp_line, ">>", 2)))
-			;//double_redir(&temp_line, );
+			double_redir(&temp_line, fd_pipe, env);
 		else if (!(ft_strncmp(temp_line, ">", 1)))
 			forward_redir(&temp_line, fd_pipe, env);
 		else if (!(ft_strncmp(temp_line, "<", 1)))
-			;//back_redir(&temp_line, );
+			;//back_redir(&temp_line, fd_pipe, env);
 		else
 			break ;
 	}
