4eaa3cbbe7ea5b9a9763f125f329c4f4f87d6497
Freely Kathryne <fkathryn@si-i6.kzn.21-school.ru>
2020-10-13 10:23:48 +0300

    del change_var and del function in main_utils

--- srcs/main_utils.c
+++ srcs/main_utils.c
@@ -6,7 +6,7 @@
 /*   By: fkathryn <fkathryn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/04 10:44:46 by qtamaril          #+#    #+#             */
-/*   Updated: 2020/10/11 15:28:35 by fkathryn         ###   ########.fr       */
+/*   Updated: 2020/10/12 17:05:30 by fkathryn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,32 +24,3 @@ void	write_prompt(void)
 	ft_putstr_fd(DEFAULT, STDOUT_FILENO);
 	free(path);
 }
-
-// int				is_separator(char c)
-// {
-// 	if (c == ' ' || c == '<' || c == '>' || c == '|' || c == ';' || c == '\'' \
-// 	|| c == '\"')
-// 		return (1);
-// 	return (0);
-// }
-
-// int		print_dir(char **cmd) //tmp function
-// {
-// 	if (cmd[0][0] == '/' && cmd[0][1] != 'p')
-// 	{
-// 		ft_putstr_fd(SHELL, STDERR_FILENO);
-// 		ft_putstr_fd(cmd[0], STDERR_FILENO);
-// 		ft_putendl_fd(IS_A_DIR, STDERR_FILENO);
-// 		ft_free_strstr(cmd);
-// 		return (1);
-// 	}
-// 	else if (cmd[0][0] == '/' && cmd[0][1] == 'p')
-// 	{
-// 		ft_putstr_fd(SHELL, STDERR_FILENO);
-// 		ft_putstr_fd(cmd[0], STDERR_FILENO);
-// 		ft_putendl_fd(P_DEN, STDERR_FILENO);
-// 		ft_free_strstr(cmd);
-// 		return (1);
-// 	}
-// 	return (0);
-// }
